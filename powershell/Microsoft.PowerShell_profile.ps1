fd4b33f6b4f9982e6ffe9c44b6ce9de7c04fd84f
_notSalt <joshuakoh303@gmail.com>
2025-10-28 14:03:50 +0800

    update powershell profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,20 @@
-# Shows navigable menu of all options when hitting Tab
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+# Disable PowerShell update check
+$env:POWERSHELL_UPDATECHECK = 'Off'
+
+# Starship
+Invoke-Expression (&starship init powershell)
+
+# PSReadLine configuration for better command-line editing
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -BellStyle None
 
 # Terminal Icons
-Import-Module Terminal-Icons
+Import-Module -Name Terminal-Icons
+
+# Key handlers
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
